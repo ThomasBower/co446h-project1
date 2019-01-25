86053e5a62efb5c6edb7e8bc1e4965a8539ddab1
andrewhill <andrew.hill15@imperial.ac.uk>
2019-01-25 03:33:42 +0000

    csp first real check done - script-src and object-src must be present, anf if they're not, then default-src must be present

--- public/csp.js
+++ public/csp.js
@@ -1,27 +1,85 @@
-messengerCSP = `Content-Security-Policy: default-src * data: blob:;script-src *.facebook.com *.fbcdn.net *.facebook.net *.google-analytics.com *.virtualearth.net *.google.com 127.0.0.1:* *.spotilocal.com:* 'unsafe-inline' 'unsafe-eval' blob: data: 'self' *.messenger.com;style-src data: blob: 'unsafe-inline' * *.messenger.com;connect-src *.facebook.com facebook.com *.fbcdn.net *.facebook.net *.spotilocal.com:* wss://*.facebook.com:* https://fb.scanandcleanlocal.com:* attachment.fbsbx.com ws://localhost:* blob: *.cdninstagram.com 'self' *.messenger.com wss://*.messenger.com:*;font-src *.messenger.com *.facebook.com static.xx.fbcdn.net data:;`;
+let messengerCSP = `default-src * data: blob:;script-src *.facebook.com *.fbcdn.net *.facebook.net *.google-analytics.com *.virtualearth.net *.google.com 127.0.0.1:* *.spotilocal.com:* 'unsafe-inline' 'unsafe-eval' blob: data: 'self' *.messenger.com;style-src data: blob: 'unsafe-inline' * *.messenger.com;connect-src *.facebook.com facebook.com *.fbcdn.net *.facebook.net *.spotilocal.com:* wss://*.facebook.com:* https://fb.scanandcleanlocal.com:* attachment.fbsbx.com ws://localhost:* blob: *.cdninstagram.com 'self' *.messenger.com wss://*.messenger.com:*;font-src *.messenger.com *.facebook.com static.xx.fbcdn.net data:;`;
+let noDefaultOneStar = "img-src *; style-src: 'self'";
 
-// result for Content-Security-Policy: default-src *; script-src 'self' google.com
-// {
-//      default-src: ["*"],
-//      script-src: ["'self", "google.com"]
-// }
-let processCSP = function (cspString) {
-    let workingString = cspString;
-    let result = {};
-    workingString = workingString.replace("Content-Security-Policy:", "");
-    let cspModes = workingString.split(";");
+const PASS_RESULT = {
+    severity: 0
+};
+
+// link=https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+function checkFunction(testCspString) { // TODO this won't be the actual args
+     //TODO GET CSP-STRING FROM ACTUAL SITE
+    let cspString = testCspString;
+    if(false) { // TODO: IF (CSP DOES NOT EXIST)
+        // if CSP does not exist, then no point in doing any more checks on it!
+        return {
+            severity: 10,
+            remedy: "You do not have a Content Security Policy," +
+                "please refer to the link to find more" +
+                "about how to create one and why you should."
+        };
+    }
+
+    // -- PROCESS CSP INTO OBJ -------
+    // variable 'csp' - for CSP below:
+    // default-src *; script-src 'self' google.com
+    // would be:
+    // {
+    //      default-src: ["*"],
+    //      script-src: ["'self", "google.com"]
+    // }
+    let csp = {};
+    let cspModes = cspString.trim().split(";");
     cspModes.forEach(function (mode) {
-        let [operator, ...arguments] = mode.split(/\s+/);
-        result[operator] = arguments;
+        let [operator, ...arguments] = mode.trim().split(/\s+/);
+        csp[operator] = arguments;
     });
-    return result;
-};
+    // --------------------------------
+    console.log(csp);
+    let maxSeverity = 0;
+    let failures = [];
 
-let checkCSP = function(cspString) {
-    let csp = processCSP(cspString);
+    // --- CHECK -----
+    // if a CSP has script-src it should also have object-src
+    // if it has neither, it should have a default-src
+    if(('script-src' in csp) && !('object-src' in csp)) {
+        maxSeverity = Math.max(maxSeverity, 8);
+        failures.push(
+            `<li class="critical">
+                You have a <code>script-src</code> directive but not a <code>object-src</code> directive.
+                This can leave you open to XSS attacks, because malicious code in a less secure policy
+                can bypass the more secure policy, if part of <code>&lt;script&gt;</code> or <code>&lt;object>&gt;</code> tags. 
+            </li>`);
+    } else if(!('script-src' in csp) && ('object-src' in csp)) {
+        maxSeverity = Math.max(maxSeverity, 8);
+        failures.push(
+            `<li class="critical">
+                You have a <code>object-src</code> directive but not a <code>script-src</code> directive.
+                This can leave you open to XSS attacks, because malicious code in a less secure policy
+                can bypass the more secure policy, if part of <code>&lt;script&gt;</code> or <code>&lt;object>&gt;</code> tags. 
+            </li>`);
+    } else if(!('script-src' in csp) && !('object-src' in csp) && !('default-src' in csp)) {
+        maxSeverity = Math.max(maxSeverity, 8);
+        failures.push(
+            `<li class="critical">
+                You have no <code>object-src</code>, <code>script-src</code> or <code>default-src</code>directives.
+                This can leave you open to XSS attacks,
+                as you have no policy covering the content of <code>&lt;script&gt;</code> or <code>&lt;object>&gt;</code> tags. 
+            </li>`);
+    }
 
-    let listOfResults = [];
-    return csp;
-};
+    // --- RETURN RESULTS -----
+    if(failures.length === 0) {
+        return PASS_RESULT;
+    } else {
+        return {
+            severity: maxSeverity,
+            remedy: `
+                <p>You have the following problem${failures.length === 1 ? "s" : ""} with your Content Security Policy</p>
+                <ul class="failures">${failures.join()}</ul>
+                `
+        };
+    }
+}
 
-console.log(checkCSP(messengerCSP));
+//console.log(checkFunction(messengerCSP));
+console.log(checkFunction(noDefaultOneStar));
