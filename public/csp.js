a0ff843aec9715ba1bc5bd64711d6546aaeff253
andrewhill <andrew.hill15@imperial.ac.uk>
2019-01-25 01:37:28 +0000

    updated to store functions in variables (so we can embed into checkFunction later)

--- public/csp.js
+++ public/csp.js
@@ -1,20 +1,11 @@
 messengerCSP = `Content-Security-Policy: default-src * data: blob:;script-src *.facebook.com *.fbcdn.net *.facebook.net *.google-analytics.com *.virtualearth.net *.google.com 127.0.0.1:* *.spotilocal.com:* 'unsafe-inline' 'unsafe-eval' blob: data: 'self' *.messenger.com;style-src data: blob: 'unsafe-inline' * *.messenger.com;connect-src *.facebook.com facebook.com *.fbcdn.net *.facebook.net *.spotilocal.com:* wss://*.facebook.com:* https://fb.scanandcleanlocal.com:* attachment.fbsbx.com ws://localhost:* blob: *.cdninstagram.com 'self' *.messenger.com wss://*.messenger.com:*;font-src *.messenger.com *.facebook.com static.xx.fbcdn.net data:;`;
 
-console.log(checkCSP(messengerCSP));
-
-function checkCSP(cspString) {
-    let csp = processCSP(cspString);
-
-    let listOfResults = [];
-    return csp;
-}
-
 // result for Content-Security-Policy: default-src *; script-src 'self' google.com
 // {
 //      default-src: ["*"],
 //      script-src: ["'self", "google.com"]
 // }
-function processCSP(cspString) {
+let processCSP = function (cspString) {
     let workingString = cspString;
     let result = {};
     workingString = workingString.replace("Content-Security-Policy:", "");
@@ -24,4 +15,13 @@ function processCSP(cspString) {
         result[operator] = arguments;
     });
     return result;
-}
+};
+
+let checkCSP = function(cspString) {
+    let csp = processCSP(cspString);
+
+    let listOfResults = [];
+    return csp;
+};
+
+console.log(checkCSP(messengerCSP));
