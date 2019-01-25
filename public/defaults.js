4738eb8e58517322ed57609fb59a2afdbb2f0e81
Thomas Bower <me@thomasbower.net>
2019-01-25 03:03:16 +0000

    Refactor existing rules and add new SRI rule

--- public/defaults.js
+++ public/defaults.js
@@ -31,18 +31,18 @@ const defaultRules = [{
     link: 'https://developer.mozilla.org/en-US/docs/Security/Mixed_content#Mixed_active_content',
     context: PAGE_CONTEXT,
     checkFunction() {
-        const PASS_RESULT = {severity: 0};
+        const PASS_RESULT = { severity: 0 };
         if (location.protocol !== 'https:') return PASS_RESULT;
-        let tags = document.querySelectorAll('script[src], form[action],'
-            + 'iframe[src], embed[src], source[src], param[value], a[href]');
+        let tags = document.querySelectorAll('script[src], form[action], iframe[src], embed[src], source[src], param[value], a[href]');
+        let failures = "";
         for (let tag of tags) {
             let source;
-            switch (tag.tagName) {
+            switch (tag.tagName.toLowerCase()) {
                 case 'script':
                 case 'iframe':
                 case 'embed':
                 case 'source':
-                    source = tag.getAttribute('src');
+                    source = tag.src;
                     break;
                 case 'form':
                     source = tag.getAttribute('action');
@@ -53,16 +53,20 @@ const defaultRules = [{
                 case 'a':
                     source = tag.getAttribute('href');
             }
-            if (!source) {
-                continue;
-            }
+            if (!source) continue;
             if (source.split('/')[0] === 'http:') {
-                return {
-                    severity: 6
-                }
+                failures += `
+                    <li class="critical"><code>${getCSSPath(tag)}</code> links to <code>${source}</code>. Change this to <code>${'https:' + source.substring(5)}</code> to avoid mixed active content.</li>
+                `
             }
         }
-        return PASS_RESULT;
+        return failures ? {
+            severity: 6,
+            remedy: `
+                <p>Resources on HTTPS pages should be loaded over HTTPS. The following resources are loaded over HTTP:</p>
+                <ul class="failures">${failures}</ul>
+            `
+        } : PASS_RESULT;
     }
 }, {
     name: 'No Mixed Passive Content',
@@ -75,21 +79,19 @@ const defaultRules = [{
         let tags = document.querySelectorAll('img[src], object[data], audio[src], video[src]');
         let failures = "";
         for (let tag of tags) {
-            let source = tag.tagName !== 'object' ? tag.getAttribute('src') : tag.getAttribute('data');
+            let source = tag.tagName.toLowerCase() !== 'object' ? tag.src : tag.getAttribute('data');
             if (source.split('/')[0] === 'http:') {
                 failures += `
-                    <li><code>${getCSSPath(tag)}</code> links to <code>${source}</code>. Change this to <code>${'https:' + source.substring(5)}</code></li>
+                    <li class="non-critical"><code>${getCSSPath(tag)}</code> links to <code>${source}</code>. Change this to <code>${'https:' + source.substring(5)}</code></li>
                 `
             }
         }
-        if (!failures) return PASS_RESULT;
-        return {
+        return failures ? {
             severity: 10,
-            remedy: `
-                <p>The following tags link to insecure media. To fix, please change the sources as specified below.</p>
-                <ul>${failures}</ul>
+            remedy: `<p>The following tags link to insecure media. To fix, please change the sources as specified below.</p>
+                     <ul class="failures">${failures}</ul>
             `
-        }
+        } : PASS_RESULT;
     }
 }, {
     name: 'CSRF Protection',
@@ -97,7 +99,7 @@ const defaultRules = [{
     link: 'https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet',
     context: PAGE_CONTEXT,
     checkFunction() {
-        const PASS_RESULT = {severity: 0};
+        const PASS_RESULT = { severity: 0 };
         let forms = document.querySelectorAll('form[action]');
         if (!forms) return PASS_RESULT;
         let failures = "", severity = 0;
@@ -117,14 +119,41 @@ const defaultRules = [{
                              not include a CSRF token.</li>`;
             }
         }
-        if (!failures) return PASS_RESULT;
-        return {
+        return failures ? {
             severity,
             remedy: `
                 <p>The following forms might not contain a CSRF token, leaving them vulnerable to cross-site request forgery:</p>
                 <ul class="failures">${failures}</ul>
             `
+        } : PASS_RESULT;
+    }
+}, {
+    name: 'Subresource Integrity',
+    description: '',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity',
+    context: PAGE_CONTEXT,
+    checkFunction() {
+        log("YAAT")
+        let tags = document.querySelectorAll('link, script');
+        let failures = "";
+        for (let tag of tags) {
+            let source = tag.tagName.toLowerCase() === 'script' ? tag.src : tag.href;
+            if (!source) continue;
+            log("HERE");
+            if (location.host !== source.split('/')[2]) {
+                failures += `<li class="non-critical"><code>${getCSSPath(tag)}</code> for source <code>${source}</code>
+                             does not contain an <code>integrity</code> attribute. You can fix this by adding generating
+                             a hash <a href="https://www.srihash.org" target="_blank">here</a>.</li>`;
+            }
         }
+        return failures ? {
+            severity: 2,
+            remedy: `
+                <p>External sources should include an integrity tag to avoid malicious code injection.
+                The following tags contain external sources without an integrity tag:</p>
+                <ul class="failures">${failures}</ul>
+            `
+        } : { severity: 0 };
     }
 }];
 
